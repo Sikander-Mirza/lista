d13ac54ac7af78e5ddc4149a96a91328ea586416
Sikander Mirza <sikandersunny2017@gmail.com>
2026-03-13 01:22:27 +0500

    webp

--- src/Screens/404NotFound/NotFound.jsx
+++ src/Screens/404NotFound/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from '../../assets/webp.png'
+import Logo from '../../assets/NewlistaLogo.webp'
 
 export default function NotFound() {
   return (
